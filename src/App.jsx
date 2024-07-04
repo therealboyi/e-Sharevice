097e8d0c748b94fa4ade03cf2def0acf78298417
BOYI <user@BOYIs-MacBook-Air.local>
2024-07-04 04:18:29 -0400

    Defined mixins, variables, fonts and globals

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <h1 className="app__title">Hello, React!</h1>
+    <p className="app__description">This is a basic React app setup with SCSS.</p>
+  </div>
+);
+
+export default App;
