3bf8604d1fe0ea61e69a8ae82ed5eb5d373e8f5f
BOYI <user@BOYIs-MacBook-Air.local>
2024-07-14 22:02:53 -0400

    Added responsive font styling to profile and savedpages

--- src/pages/SavedPage/SavedPage.jsx
+++ src/pages/SavedPage/SavedPage.jsx
@@ -5,44 +5,44 @@ import "./SavedPage.scss";
 
 const savedItems = [
   {
-    city: "Edmonton",
-    host: "SWISH Rentals",
+    title: "SWISH Rentals",
+    type: "Service",
     dates: "May 3, 2024 – May 5, 2024",
     imgSrc: "/src/assets/images/messi.png",
   },
   {
-    city: "Luton",
-    host: "Kallum",
+    title: "Barber",
+    type: "Service",
     dates: "Mar. 19, 2023 – Mar. 30, 2023",
     imgSrc: "/src/assets/images/ronaldo.png",
   },
   {
-    city: "Vancouver",
-    host: "May",
+    title: "Book",
+    type: "Item",
     dates: "May 21, 2021 – May 25, 2021",
     imgSrc: "/src/assets/images/mbappe.png",
   },
   {
-    city: "Edmonton",
-    host: "Janelle & AJ",
+    title: "Basketball",
+    type: "Tool",
     dates: "May 12, 2021 – May 13, 2021",
     imgSrc: "/src/assets/images/durant.png",
   },
   {
-    city: "Winnipeg",
-    host: "E.Q",
+    title: "Cleaning",
+    type: "Service",
     dates: "Jan. 15, 2021 – Jan. 17, 2021",
     imgSrc: "/src/assets/images/lebron.png",
   },
   {
-    city: "Toronto",
-    host: "Lyla",
+    title: "Mouse",
+    type: "Tool",
     dates: "Mar. 16, 2020 – Mar. 20, 2020",
     imgSrc: "/src/assets/images/giannis.png",
   },
   {
-    city: "Winnipeg",
-    host: "Devin",
+    title: "Chef",
+    type: "Service",
     dates: "Jun. 5, 2019 – Jun. 7, 2019",
     imgSrc: "/src/assets/images/curry.png",
   },
@@ -60,12 +60,12 @@ const SavedPage = () => {
               <li key={index} className="saved-page__item">
                 <img
                   src={item.imgSrc}
-                  alt={item.city}
+                  alt={item.title}
                   className="saved-page__image"
                 />
                 <div className="saved-page__info">
-                  <h2 className="saved-page__city">{item.city}</h2>
-                  <p className="saved-page__host">Hosted by {item.host}</p>
+                  <h2 className="saved-page__title">{item.title}</h2>
+                  <p className="saved-page__type">{item.type}</p>
                   <p className="saved-page__dates">{item.dates}</p>
                 </div>
               </li>
